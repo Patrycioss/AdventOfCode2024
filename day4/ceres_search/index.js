258c199d6058be128e49d41665375fca99ab0c83
Patrick Schuur <patrickschuurdev@gmail.com>
2024-12-09 10:25:10 +0100

    Cleaned up a bit

--- day4/ceres_search/index.js
+++ day4/ceres_search/index.js
@@ -23,11 +23,16 @@ class Occurrence {
     }
 }
 
-
 let grid = []
 let columns = 0
 let rows = 0
-let occurrences = []
+
+const DIRECTIONS = {
+    BR: new Vector2(1, 1),
+    BL: new Vector2(-1, 1),
+    TL: new Vector2(-1, -1),
+    TR: new Vector2(1, -1),
+}
 
 start();
 
@@ -47,127 +52,66 @@ function make_grid(input) {
     }
 }
 
-function is_valid(x, y) {
-    return (x >= 0 && x < columns) && (y >= 0 && y < rows);
+function is_valid(pos) {
+    return (pos.x >= 0 && pos.x < columns) && (pos.y >= 0 && pos.y < rows);
 }
 
+function doesOccur(pos, dir, word, occurrences) {
 
-function try_direction(fromX, fromY, dirX, dirY, word) {
-
-    for (let index = 1; index < word.length; index++) {
-        const x = fromX + (dirX * index);
-        const y = fromY + (dirY * index);
-        if (is_valid(x, y)) {
-            if (!(grid[x][y] === word[index])) {
-                return false;
+    for (let i = 1; i < word.length; i++) {
+        const newPos = new Vector2(pos.x + i * dir.x, pos.y + i * dir.y);
+        if (is_valid(newPos)) {
+            if (!(grid[newPos.x][newPos.y] === word[i])) {
+                return;
             }
         } else {
-            return false;
+            return;
         }
     }
-
-    return true;
-}
-
-function try_direction_and_store(fromX, fromY, dirX, dirY, word) {
-
-    for (let index = 1; index < word.length; index++) {
-        const x = fromX + (dirX * index);
-        const y = fromY + (dirY * index);
-        if (is_valid(x, y)) {
-            if (!(grid[x][y] === word[index])) {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-
 
     let occurrence = new Occurrence();
-    occurrence.direction = new Vector2(dirX, dirY);
+    occurrence.direction = new Vector2(dir.x, dir.y);
 
     for (let i = 0; i < word.length; i++) {
-        occurrence.positions[i] = new Vector2(fromX + i * dirX, fromY + i * dirY);
+        occurrence.positions[i] = new Vector2(pos.x + i * dir.x, pos.y + i * dir.y);
     }
 
     occurrences.push(occurrence);
-    return true;
 }
 
-
-function try_all_directions(x, y, word, onFound) {
-    if (try_direction(x, y, -1, -1, word)) onFound();
-    if (try_direction(x, y, 0, -1, word)) onFound(); // T
-    if (try_direction(x, y, 1, -1, word)) onFound(); // TR
-    if (try_direction(x, y, 1, 0, word)) onFound(); // R
-    if (try_direction(x, y, 1, 1, word)) onFound(); // BR
-    if (try_direction(x, y, 0, 1, word)) onFound(); // B
-    if (try_direction(x, y, -1, 1, word)) onFound(); // BL
-    if (try_direction(x, y, -1, 0, word)) onFound(); // L
+function find_all_occurrences(pos, word, occurrences) {
+    doesOccur(pos, new Vector2(-1, -1), word, occurrences);
+    doesOccur(pos, new Vector2(0, -1), word, occurrences); // T
+    doesOccur(pos, new Vector2(1, -1), word, occurrences); // TR
+    doesOccur(pos, new Vector2(1, 0), word, occurrences); // R
+    doesOccur(pos, new Vector2(1, 1), word, occurrences); // BR
+    doesOccur(pos, new Vector2(0, 1), word, occurrences); // B
+    doesOccur(pos, new Vector2(-1, 1), word, occurrences); // BL
+    doesOccur(pos, new Vector2(-1, 0), word, occurrences); // L
 }
 
-function try_all_directions_and_store(x, y, word, onFound) {
-    if (try_direction_and_store(x, y, -1, -1, word)) onFound();
-    if (try_direction_and_store(x, y, 0, -1, word)) onFound(); // T
-    if (try_direction_and_store(x, y, 1, -1, word)) onFound(); // TR
-    if (try_direction_and_store(x, y, 1, 0, word)) onFound(); // R
-    if (try_direction_and_store(x, y, 1, 1, word)) onFound(); // BR
-    if (try_direction_and_store(x, y, 0, 1, word)) onFound(); // B
-    if (try_direction_and_store(x, y, -1, 1, word)) onFound(); // BL
-    if (try_direction_and_store(x, y, -1, 0, word)) onFound(); // L
-}
-
-function count_word(word) {
-    let count = 0;
+function find_occurrences(word) {
+    let occurrences = [];
     for (let x = 0; x < columns; x++) {
         for (let y = 0; y < rows; y++) {
             if (grid[x][y] === word[0]) {
-                try_all_directions(x, y, word, () => {
-                    count++;
-                });
+                find_all_occurrences(new Vector2(x, y), word, occurrences);
             }
         }
     }
-    return count;
-}
-
-function count_word_and_store(word) {
-    let count = 0;
-    for (let x = 0; x < columns; x++) {
-        for (let y = 0; y < rows; y++) {
-            if (grid[x][y] === word[0]) {
-                try_all_directions_and_store(x, y, word, () => {
-                    count++;
-                });
-            }
-        }
-    }
-    return count;
+    return occurrences;
 }
 
 function part1() {
-    console.log(`Part 1 Count: ${count_word("XMAS")}`);
-    occurrences = [];
-}
-
-const DIRECTIONS = {
-    BR: new Vector2(1, 1),
-    BL: new Vector2(-1, 1),
-    TL: new Vector2(-1, -1),
-    TR: new Vector2(1, -1),
+    console.log(`Part 1 Count: ${find_occurrences("XMAS").length}`);
 }
 
 function part2() {
-
     const word = "MAS";
-    count_word_and_store(word)
+    const occurrences = find_occurrences(word)
     const halfLength = Math.floor(word.length / 2);
 
     let final_count = 0;
-
-    console.log(occurrences[0].direction);
-    console.log(occurrences[1].direction);
 
     for (let i = occurrences.length - 1; i >= 0; i--) {
         for (let j = i - 1; j >= 0; j--) {
@@ -201,12 +145,7 @@ async function start() {
         }
 
         make_grid(data)
-        // part1();
+        part1();
         part2();
     });
 }
-
-
-
-
-
